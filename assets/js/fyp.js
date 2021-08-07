6cacf564acb9d52a493915229732fb3c1922fe70
CJ Trahey <trahey.03@gmail.com>
2021-08-07 13:58:54 -0400

    added framing tools to some of the scripts for the homepage links

---
+++ assets/js/fyp.js
@@ -0,0 +1,3 @@
+// expand images on hover, thats it for the most part. This is mostly a pre-made HTML 
+// instead of something dynamically created. In the future though, this could also be 
+// something dynamically created based on a survey the user fills out upon signing up.
